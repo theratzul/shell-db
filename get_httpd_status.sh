765d27e49624d2a692c552cb3d70989f12c0e3e0
Bogdan <bogdan@localhost.localdomain>
2024-02-10 21:34:19 +0200

    fix/output-read-command

--- get_httpd_status.sh
+++ get_httpd_status.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-status=$(systemctl status apache2 | awk 'NR==3 { print $2}')
+read -p "Enter your service name: " web
+status=$(systemctl status $web | awk 'NR==3 { print $2}')
 echo -e "The stats is: \033[92m$status\033"
